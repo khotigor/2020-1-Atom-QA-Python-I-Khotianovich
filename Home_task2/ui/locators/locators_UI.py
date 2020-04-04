8084fb9370f4dae94af4d6c769eefe9e37ef0d8e
khotigor <khotigor177@gmail.com>
2020-04-05 00:48:00 +0300

    locators renamed, bug fixed

--- Home_task2/ui/locators/locators_UI.py
+++ Home_task2/ui/locators/locators_UI.py
@@ -2,28 +2,28 @@ from selenium.webdriver.common.by import By
 
 
 class AuthorizationLocators:
-    EMAIL_FIELD = (By.NAME, "email")
-    PASSWORD_FIELD = (By.NAME, "password")
+    EMAIL_FIELD = (By.NAME, 'email')
+    PASSWORD_FIELD = (By.NAME, 'password')
 
     FULL_PENETRATION_BUTTON = (
-        By.CLASS_NAME, "responseHead-module-button-1BMAy4")
+        By.CLASS_NAME, 'responseHead-module-button-1BMAy4')
 
-    CHECK_AUTH_SUCCESS = (By.CLASS_NAME, "rightMenu-module-rightMenu-2GKiG5")
-    CHECK_AUTH_FAILED = (By.CLASS_NAME, "formMsg_text")
+    CHECK_AUTH_SUCCESS = (By.CLASS_NAME, 'rightMenu-module-rightMenu-2GKiG5')
+    CHECK_AUTH_FAILED = (By.CLASS_NAME, 'formMsg_text')
 
 
 class CreateAdvertisingCampaignLocators:
-    CREATE_ADVERTISING_CAMPAIGN = (By.XPATH,
-                                   "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div/div[3]/div/div[3]/span/a")
+    CREATE_ADVERTISING_CAMPAIGN = (By.PARTIAL_LINK_TEXT, 'создайте')
+    CREATE_ADVERTISING_CAMPAIGN_NOT_FIRST = (
+        By.XPATH, '//span[contains(@data-loc-ru, "Создать кампанию")]')
     TRAFFIC_TYPE_CAMPAIGN = (By.XPATH, '//div[contains(text(), "Трафик")]')
-    LINK_EDIT = (By.XPATH,
-                 "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div/div[4]/div[2]/div/div[2]/div[1]/div[1]/div/div/input")
-    NAME_EDIT = (By.XPATH,
-                 "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div/div[4]/div[2]/div/div[7]/div/div[2]/div/div[2]/input")
-    DOWNLOAD = (By.XPATH,
-                '//input[contains(@class, "input__inp input__inp_file js-form-element")]')
+    LINK_EDIT = (By.XPATH, '//div[contains(@class, "input__wrap")]/' +
+                 'input[contains(@placeholder, "Введите ссылку")]')
+    NAME_EDIT = (By.XPATH, '//div[2]/input[contains(@class, ' +
+                 '"input__inp js-form-element")]')
+    DOWNLOAD = (By.XPATH, '//input[contains(@class, ' +
+                '"input__inp input__inp_file js-form-element")]')
     BANNER = (By.ID, '192')
     BUTTON_CREATE = (By.XPATH, '//div[contains(text(), "Создать кампанию")]')
-
     CHECK = (By.XPATH,
-             "/html/body/div[1]/div[2]/div[1]/div/div[2]/div[2]/div[2]/div/div[1]/div[2]/div/div[3]/div/div[2]/div[3]/div/div/div[1]/table/tbody/tr/td/div/div/table/tbody/tr/td[1]/div/div/div/a")
+             '//a[contains(@class, "campaigns-tbl-cell__campaign-name")]')
--- Home_task2/ui/pages/AuthorizedPage.py
+++ Home_task2/ui/pages/AuthorizedPage.py
@@ -1,16 +1,22 @@
+import random
 import time
 
 from ui.pages.BasePage import BasePage
 from ui.locators.locators_UI import CreateAdvertisingCampaignLocators
 
-NAME_OF_CAMPAIGN = "I love Freddie"
+NAME_OF_CAMPAIGN = "I love Freddie" + str(random.randint(-1000, 1000))
 
 
 class AuthorizedPage(BasePage):
     locators = CreateAdvertisingCampaignLocators()
 
     def create_campaign(self, file_to_download):
-        self.click(self.locators.CREATE_ADVERTISING_CAMPAIGN)
+
+        try:
+            self.click(self.locators.CREATE_ADVERTISING_CAMPAIGN_NOT_FIRST)
+        except:
+            self.click(self.locators.CREATE_ADVERTISING_CAMPAIGN)
+
         self.click(self.locators.TRAFFIC_TYPE_CAMPAIGN)
         self.input_data("mail.ru", self.locators.LINK_EDIT)
         self.input_data(NAME_OF_CAMPAIGN, self.locators.NAME_EDIT)
