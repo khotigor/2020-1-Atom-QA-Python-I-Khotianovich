276ee2cc66f5981bcac309977ab26b603b5d806b
khotigor <khotigor177@gmail.com>
2020-04-06 14:08:22 +0300

    API authorization is done!!!

--- Home_task2/api/Client.py
+++ Home_task2/api/Client.py
@@ -9,25 +9,38 @@ class ResponseStatusException(Exception):
 
 class Client:
 
-    def __init__(self):
+    def __init__(self, user, password):
         self.base_url = 'https://target.my.com'
+        self.session = requests.Session()
+        self.user = user
+        self.password = password
+        self.csrf_token = None
 
-    def _request(self, method, location, headers=None, params=None):
-
+    def _request(self, method, location, headers=None, data=None):
         url = urljoin(self.base_url, location)
-
-        response = requests.request(method, url, headers=headers, params=params)
-
-        # if response.status_code != status_code:
-        #     raise ResponseStatusException(
-        #         'Got {code} for url {url}'.format(code=response.status_code,
-        #                                           url=url))
+        response = self.session.request(method, url, headers=headers,
+                                        data=data)
         return response
 
     def get_token(self):
         location = 'csrf/'
-        result = self._request('GET', location).headers
-        print(result)
+        headers = self._request('GET', location).headers
+        self.csrf_token = headers['Set-Cookie'].split(';')[0].split('=')[-1]
 
     def login(self):
-        pass
+        location = 'https://auth-ac.my.com/auth?lang=ru&nosavelogin=0'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Referer': 'https://target.my.com/'
+        }
+
+        data = {
+            'csrfmiddlewaretoken': self.csrf_token,
+            'login': self.user,
+            'password': self.password,
+            'continue': 'https://account.my.com/login_continue/?continue=https%3A%2F%2Faccount.my.com',
+            'failure': 'https://account.my.com/login/?continue=https%3A%2F%2Faccount.my.com',
+        }
+        response = self._request('POST', location, headers=headers,
+                                 data=data).url
+        return response
