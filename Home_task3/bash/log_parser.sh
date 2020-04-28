bfa763f61d2406ccf63e05f3c5a41ce89d5d0878
khotigor <khotigor177@gmail.com>
2020-04-28 11:10:11 +0300

    bash second part of the task finished

--- Home_task3/bash/log_parser.sh
+++ Home_task3/bash/log_parser.sh
@@ -5,16 +5,55 @@ echo ""
 read path
 echo "The path is $path"
 echo ""
+filetype="*.log"
 while [ -n "$1" ]
 do
 case "$1" in
--a) wc -l $path > ./res/res-a.txt
+
+-a)
+  if [ -f "$path" ]; then
+      echo "$path" > ./res/res-a.txt
+      cat "$path" | wc -l >> ./res/res-a.txt
+  elif [ -d "$path" ]; then
+      echo "$path/$filetype" > ./res/res-a.txt
+      cat $path/$filetype | wc -l >> ./res/res-a.txt
+  else
+    echo "Error: $path is bad"
+    echo ""
+  fi
   shift;;
--b) echo "Found the -b option" ;;
--c) echo "Found the -c option" ;;
+
+
+-b)
+  request_methods=("GET" "HEAD" "POST" "PUT" "DELETE" "TRACE" "OPTIONS" "CONNECT" "PATCH")
+  if [ -f "$path" ]; then
+      echo "$path" > ./res/res-b.txt
+      # shellcheck disable=SC2068
+      for t in ${request_methods[@]}; do
+        echo "$t"  >> ./res/res-b.txt
+        # shellcheck disable=SC2126
+        cat "$path" | grep "$t" | wc -l >> ./res/res-b.txt
+      done
+
+  elif [ -d "$path" ]; then
+      echo "$path/$filetype" > ./res/res-b.txt
+      # shellcheck disable=SC2068
+      for t in ${request_methods[@]}; do
+        echo "$t"  >> ./res/res-b.txt
+        # shellcheck disable=SC2126
+        cat $path/$filetype | grep "$t" | wc -l >> ./res/res-b.txt
+      done
+
+  else
+    echo "Error: $path is bad"
+    echo ""
+  fi
+  shift;;
+
+
 *) echo "$1 is not an option" ;;
 esac
-echo "Done!"
+echo "Finished!"
 echo ""
 shift
 done
--- Home_task3/data/readmeData.md
+++ Home_task3/data/readmeData.md
@@ -1,4 +1,4 @@
 Dump dir
 - for testing with few files
-- readmeData.md is not a .log file, so we will take get it
-- nginx_logs_1.log and nginx_logs_2.log are just a parts of nginx_logs.log 
+- readmeData.md is not a .log file, so we will not take it
+- nginx_logs_1.log and nginx_logs_2.log are .log files
